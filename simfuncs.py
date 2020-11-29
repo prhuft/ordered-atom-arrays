78f1dd3572a3dd74a65598a89ec9cc59f28021ce
prhuft <31295508+prhuft@users.noreply.github.com>
2020-11-28 20:38:47 -0600

    vector solution write to file bug fix, build 2-excitation hamiltonian, started .py script simulation

--- simfuncs.py
+++ simfuncs.py
@@ -11,14 +11,20 @@ import numpy as np
 
 def soln_to_csv(fname, data, labels):
     """
-    fname: myfile.csv
-    data: a list or array of equal length lists or arrays of data
-    labels: a label describing each list in data
+    Args:
+        fname: myfile.csv
+        data: a list or array of equal length lists or arrays of data
+        labels: a label describing each list in data
     
-    e.g. 
+        e.g., 
         data = [array([1,2,3]), array([2,4,6], array([1,4,9]]
         labels = ['x', '2x', 'x^2']
     """
+    
+    if type(data) == np.ndarray:
+    # listify to avoid malforming strings
+        data = [d for d in data]
+    
     with open(fname, 'w', newline='') as f:
         writer = csv.writer(f, delimiter=',')
 
@@ -29,15 +35,15 @@ def soln_to_csv(fname, data, labels):
 
 def soln_from_csv(fname):
     """
-    fname: myfile.csv
-    
-    returns:
+    Args:
+        fname: myfile.csv
+    Returns:
         data: an array of equal length arrays of data
         labels: a label describing each array in data
     
-    e.g. 
-        data = [array([1,2,3]), array([2,4,6], array([1,4,9]]
-        labels = ['x', '2x', 'x^2']
+        e.g. 
+            data = [array([1,2,3]), array([2,4,6], array([1,4,9]]
+            labels = ['x', '2x', 'x^2']
     """
     labels = []
     data = []
@@ -47,7 +53,12 @@ def soln_from_csv(fname):
 
         for row in reader:
             labels.append(row[0])
-            data.append(np.array(row[1:], float))
+            try:
+                data.append(np.array([complex(x) for x in row[1:]]))
+            except (ValueError,TypeError) as e:
+                print(e)
+                print(f"problematic row: {row}")
+                break
             
     return data, labels
     
